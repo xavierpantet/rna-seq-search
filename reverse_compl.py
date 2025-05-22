c6c0a6b01676639f57947448c6d71f3e0d22ee52
Xavier Pantet <25949462+xavierpantet@users.noreply.github.com>
2025-05-22 12:02:53 +0200

    Created inverse complement calculator for DNA/RNA sequences

---
+++ reverse_compl.py
@@ -0,0 +1,20 @@
+import argparse
+
+COMPLEMENTARY_BASES = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C',
+    ' ': '',
+}
+
+def reverse_compl(seq):
+    compl = list(map(lambda char: COMPLEMENTARY_BASES[char], seq))
+    reversed = compl[::-1]
+    return ''.join(reversed)
+
+parser = argparse.ArgumentParser(description = 'Inverse complement calculator.')
+parser.add_argument('sequence', help = 'Your sequence')
+args = parser.parse_args()
+
+print(reverse_compl(args.sequence))
